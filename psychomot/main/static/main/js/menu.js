4484eb5e4612b542fb1e0b5bd7c470e5cc329ba8
thibaultspriet <thibaultspriet@hotmail.fr>
2020-06-13 11:51:57 +0200

    Menu animation

--- psychomot/main/static/main/js/menu.js
+++ psychomot/main/static/main/js/menu.js
@@ -2,15 +2,42 @@ $(function(){
     const hamburger = $('.hamburger')
     const menu = $('.list-rubriques')
     const links = $('.list-rubriques li')
+    const menuContainer = $('.rubriques')
+
+
+    // Timeline GSAP
+    var menuTL = gsap.timeline({paused:true})
+        .to(hamburger,1,{
+            left:"unset",right:0,
+            ease : Power2.easeOut
+        })
+        .to(menuContainer,1,{
+            height:"auto",
+            ease : Power2.easeOut
+        },"-=1")
+        .to(menu,1,{
+            transform : "translateX(0)",
+            ease : Power2.easeOut
+        },"-=1")
+        .from(links,1,{
+            transform : "translateX(-100%)",
+            opacity : 0,
+            ease : Power2.easeOut,
+            delay : (index,element,all)=>{
+                return index*0.2 
+            }
+        },'-=1')
+        .reverse()
+    
     
     
     hamburger.click(function (){
-        menu.toggleClass('active')
-        $.each(links, function (index,link){
-            let delay = index*100
-            let animation =  'revealLink .5s ease ' + delay + 'ms both'
-            console.log(animation)
-            link.style.animation = animation
-        })
+        if(menuTL.reversed()){
+            menuTL.play()
+        }
+        else{
+            menuTL.reverse()
+        }
+        
     })
 })
