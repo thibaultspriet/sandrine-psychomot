ea37452405b0c24c1ec0b700e9826334fef6c2de
thibaultspriet <thibaultspriet@hotmail.fr>
2020-06-13 11:05:16 +0200

    animation with GSAP

---
+++ psychomot/main/static/main/js/icon_scroll.js
@@ -0,0 +1,26 @@
+/* gsap.registerPlugin(ScrollTrigger) */
+
+const icons = gsap.utils.toArray('.role .icon')
+
+
+function tlIcon(icon){
+    icon.tl = gsap.timeline({
+        paused : true,
+        scrollTrigger : {
+            trigger : icon,
+            start: "bottom 100%",
+            toggleActions : "restart resume resume reset",
+        }
+    }).to(icon,1,{
+        opacity : 1,
+        ease : Power2.easeOut
+    }).from(icon,2,{
+        y : "15px",
+        ease : Elastic.easeOut
+    },'-=1')
+
+}
+
+icons.forEach((icon,index)=>{
+    tlIcon(icon)
+});
--- psychomot/main/static/main/js/toggledescription.js
+++ psychomot/main/static/main/js/toggledescription.js
@@ -2,20 +2,44 @@ $(function(){
     const btns = $('.container-cross')
     const descriptions = $('.description')
     const lines2 = $('.line2')
+    const containers = $('.container-item-description')
 
 
     btns.click(function (e){
-        let description = e.target.nextElementSibling
-        descriptions.each((index,el)=>{
-            if (el !== description){
-                el.classList.remove("active")
+        let container = e.target.parentElement
+        containers.each((index,el)=>{
+            if (el !== container){
+                el.descriptionTl.reverse()
+
+            }
+            else{
+                if(el.descriptionTl.reversed()){
+                    el.descriptionTl.play()
+                }
+                else{
+                    el.descriptionTl.reverse()
+                }
             }
         })
-        lines2.css('display', 'block')
-        description.classList.toggle('active')
-        if(description.classList.contains("active")){
-            e.target.children[1].style.display="none"
-        }
-        
     })
 })
+
+// GSAP
+function tl(container,description,line2){
+    container.descriptionTl = gsap.timeline({paused:true})
+    .to(line2,.3,{opacity : 0})
+    .set(description,{height : "auto"})
+    .from(description,.5,{
+        height : 0,
+        ease : Power1.easeInOut
+    })
+    
+    container.descriptionTl.reverse()
+}
+
+gsap.utils.toArray('.container-item-description').forEach((container,index)=>{
+    var description = container.querySelector('.description')
+    var line2 = container.querySelector('.line2')
+    tl(container,description, line2)
+});
+
