dbcbd01e72a96344796c93111639cd86e003e434
thibaultspriet <thibaultspriet@hotmail.fr>
2020-12-14 19:35:45 +0100

    form submission

--- psychomot_nextjs/pages/contact.js
+++ psychomot_nextjs/pages/contact.js
@@ -4,64 +4,64 @@ import Image from "next/image"
 
 import "../static/css/contact.css"
 
-export default function Contact(){
+export default function Contact() {
 
-    return(
+    return (
         <div>
-        <Base route="contact">
-            <Head>
-                <title>Contact</title>
-            </Head>
+            <Base route="contact">
+                <Head>
+                    <title>Contact</title>
+                </Head>
 
-            <div id="page-contact">
-            <header className="cover-container">
-                <img src="/main/contact-cover.jpg" alt="contact" className="cover"/>
-            </header>
+                <div id="page-contact">
+                    <header className="cover-container">
+                        <img src="/main/contact-cover.jpg" alt="contact" className="cover" />
+                    </header>
 
-            <main>
-                <div className="main-content">
-                    <h3>Laissez moi un message !</h3>
-                    <div className="hs"></div>
-                    <p>
-                        Si vous avez des questions, merci de m’adresser un message à l’aide de ce formulaire. Je vous répondrai
-                        alors au plus vite. Vous pouvez également prendre contact directement par téléphone ou en ligne.
+                    <main>
+                        <div className="main-content">
+                            <h3>Laissez moi un message !</h3>
+                            <div className="hs"></div>
+                            <p>
+                                Si vous avez des questions, merci de m’adresser un message à l’aide de ce formulaire. Je vous répondrai
+                                alors au plus vite. Vous pouvez également prendre contact directement par téléphone ou en ligne.
                     </p>
 
-                    <form name="contact" method="POST" data-netlify="true" id="contactForm" enctype="application/x-www-form-urlencoded">
+                            <form name="contact" method="POST" data-netlify="true" id="contactForm" enctype="application/x-www-form-urlencoded">
 
-                        <label htmlFor="nom_prenom">Nom & Prénom</label>
-                        <input type="text" name="nom_prenom" required/>
-                        
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name="email" required/>
-                        
-                        <label htmlFor="sujet">Sujet</label>
-                        <input type="text" name="sujet" required/>
+                                <label htmlFor="nom_prenom">Nom & Prénom</label>
+                                <input type="text" name="nom_prenom" required />
 
-                        <label htmlFor="message">Message</label>
-                        <textarea name="message" cols="30" rows="10" required></textarea>
+                                <label htmlFor="email">Email</label>
+                                <input type="email" name="email" required />
 
-                        <input type="submit" value="Envoyer"/>
-                    </form>
+                                <label htmlFor="sujet">Sujet</label>
+                                <input type="text" name="sujet" required />
 
-                </div>
-                <aside className="infos">
-                    <h1>
-                        Sandrine Dubois <br/> Psychomotricienne
+                                <label htmlFor="message">Message</label>
+                                <textarea name="message" cols="30" rows="10" required></textarea>
+
+                                <input name="submit" type="submit" value="Envoyer" />
+                            </form>
+
+                        </div>
+                        <aside className="infos">
+                            <h1>
+                                Sandrine Dubois <br /> Psychomotricienne
                     </h1>
-                    <div className="hs"></div>
-                    <p><a className="tel secondary" href="tel:0615742382">06 08 30 28 97</a></p>
-                    <div className="adresse">
-                        <p>
-                            Cabinet de Psychomotricité <br/> Mérignies
+                            <div className="hs"></div>
+                            <p><a className="tel secondary" href="tel:0615742382">06 08 30 28 97</a></p>
+                            <div className="adresse">
+                                <p>
+                                    Cabinet de Psychomotricité <br /> Mérignies
                         </p>
-                    </div>
-                </aside>
-            </main>
-            </div>
-            
-        </Base>
-    </div>
+                            </div>
+                        </aside>
+                    </main>
+                </div>
+
+            </Base>
+        </div>
     )
-    
+
 }
--- psychomot_nextjs/pages/rendez_vous.js
+++ psychomot_nextjs/pages/rendez_vous.js
@@ -2,13 +2,13 @@ import Head from "next/head"
 import Base from "../components/Base"
 import Image from "next/image"
 
-import {useState,useEffect} from "react"
+import { useState, useEffect } from "react"
 
 import "../static/css/rendez_vous.css"
 
-export default function RendezVous({services}){
+export default function RendezVous({ services }) {
 
-    const [serviceSelected,setServiceSelected] = useState(services[0])
+    const [serviceSelected, setServiceSelected] = useState(services[0])
 
     const handleChange = (e) => {
         const label = e.target.value
@@ -16,91 +16,91 @@ export default function RendezVous({services}){
         setServiceSelected(...service)
     }
 
-    return(
+    return (
         <div>
-        <Base route="rendez_vous">
-            <Head>
-                <title>Rendez-vous</title>
-            </Head>
+            <Base route="rendez_vous">
+                <Head>
+                    <title>Rendez-vous</title>
+                </Head>
 
-            <div id="page-rdv">
-            <header className='rdv-header' style={{backgroundImage: "url('/main/plaque_cabinet.jpg')"}}>
-                <h1>Prenez Un Rendez-Vous</h1>
-                <p>Contactez moi en quelques clics et en rédigeant un message si vous le souhaitez. Je vous réponds au plus vite pour confirmer le rendez-vous.</p>
-            </header>
+                <div id="page-rdv">
+                    <header className='rdv-header' style={{ backgroundImage: "url('/main/plaque_cabinet.jpg')" }}>
+                        <h1>Prenez Un Rendez-Vous</h1>
+                        <p>Contactez moi en quelques clics et en rédigeant un message si vous le souhaitez. Je vous réponds au plus vite pour confirmer le rendez-vous.</p>
+                    </header>
 
-            <div className="form-container">
-                <form name="rendez vous" method="POST" data-netlify="true" id="rdvForm" enctype="application/x-www-form-urlencoded">
+                    <div className="form-container">
+                        <form name="rendez vous" method="POST" data-netlify="true" id="rdvForm" enctype="application/x-www-form-urlencoded">
 
 
-                    <div className="select-service">
-                        <label htmlFor="service">Choisissez un service</label>
-                        <select name="service" onChange={handleChange} required style={{backgroundImage : "url('/main/arrow-down.svg')"}}>
-                            {
-                                services.map(({label},ind) => {
-                                    return (
-                                        <option value={label} key={ind}>{label}</option>
-                                    )
-                                })
-                            }
-                        </select>
+                            <div className="select-service">
+                                <label htmlFor="service">Choisissez un service</label>
+                                <select name="service" onChange={handleChange} required style={{ backgroundImage: "url('/main/arrow-down.svg')" }}>
+                                    {
+                                        services.map(({ label }, ind) => {
+                                            return (
+                                                <option value={label} key={ind}>{label}</option>
+                                            )
+                                        })
+                                    }
+                                </select>
+                            </div>
+                            <main className="recap">
+                                <ul className="infos">
+                                    <li className="name"><span id="name">{serviceSelected.label}</span></li>
+                                    <li className="price" >Prix : <span id="price">{serviceSelected.price}</span>€</li>
+                                    <li className="duration">Durée : <span id="duration">{serviceSelected.duration}</span></li>
+                                </ul>
+                                <div className="description">
+                                    <span id="description">{serviceSelected.description}</span>
+                                </div>
+                                <div className="email">
+                                    <label htmlFor="email">Email</label>
+                                    <input type="email" name="email" required />
+                                </div>
+                                <div className="message">
+                                    <label htmlFor="message">Message</label>
+                                    <textarea name="message" cols="30" rows="10" required></textarea>
+                                </div>
+                                <input name="submit" type="submit" value="Envoyer" />
+                            </main>
+                        </form>
                     </div>
-                    <main className="recap">
-                        <ul className="infos">
-                            <li className="name"><span id="name">{serviceSelected.label}</span></li>
-                            <li className="price" >Prix : <span id="price">{serviceSelected.price}</span>€</li>
-                            <li className="duration">Durée : <span id="duration">{serviceSelected.duration}</span></li>
-                        </ul>
-                        <div className="description">
-                            <span id="description">{serviceSelected.description}</span>
-                        </div>
-                        <div className="email">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" required/>
-                        </div>
-                        <div className="message">
-                            <label htmlFor="message">Message</label>
-                            <textarea name="message" cols="30" rows="10" required></textarea>
-                        </div>
-                        <input type="submit" value="Envoyer"/>
-                    </main>
-                </form>
-            </div>
-            </div>
-            
-        </Base>
-    </div>
+                </div>
+
+            </Base>
+        </div>
     )
-    
+
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
     return {
-        props : {
-            services : [
+        props: {
+            services: [
                 {
-                    label : "Bilan Psychomoteur",
-                    price : "150",
-                    duration : "1h30",
-                    description : "Lors du premier rendez-vous, le psychomotricien r\u00e9alise un entretien qui permet de mieux comprendre la demande initiale en reprenant l\u2019histoire de vie, les diff\u00e9rentes \u00e9tapes de son d\u00e9veloppement, les difficult\u00e9s rencontr\u00e9es, les particularit\u00e9s comportementales"
+                    label: "Bilan Psychomoteur",
+                    price: "150",
+                    duration: "1h30",
+                    description: "Lors du premier rendez-vous, le psychomotricien r\u00e9alise un entretien qui permet de mieux comprendre la demande initiale en reprenant l\u2019histoire de vie, les diff\u00e9rentes \u00e9tapes de son d\u00e9veloppement, les difficult\u00e9s rencontr\u00e9es, les particularit\u00e9s comportementales"
                 },
                 {
-                    label : "Séance de psychomotricité",
-                    price : "50",
-                    duration : "1h",
-                    description : "Ateliers de psychomotricité"
-                },
-                {
-                    label : "Yoga / Sophrologie adultes",
-                    price : "50",
+                    label: "Séance de psychomotricité",
+                    price: "50",
                     duration: "1h",
-                    description : "Séance pour adultes"
+                    description: "Ateliers de psychomotricité"
                 },
                 {
-                    label : "Yoga / Sophrologie enfants",
-                    price : "40",
-                    duration : "45min",
-                    description : "Séances enfants"
+                    label: "Yoga / Sophrologie adultes",
+                    price: "50",
+                    duration: "1h",
+                    description: "Séance pour adultes"
+                },
+                {
+                    label: "Yoga / Sophrologie enfants",
+                    price: "40",
+                    duration: "45min",
+                    description: "Séances enfants"
                 }
             ]
         }
